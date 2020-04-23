75bdbd6de0375fc786952943b222c172dfd4e56d
xwmtp <xwillmtp@gmail.com>
2020-04-23 18:46:36 +0200

    Updates

---
+++ precache-manifest.b7f273e1f080877cd7e83fb805846130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e521e87067a5176514f221d9d76cac",
+    "url": "/Path-Bingo/index.html"
+  },
+  {
+    "revision": "6365d4d693b64fd50ed6",
+    "url": "/Path-Bingo/static/css/main.36e328f6.chunk.css"
+  },
+  {
+    "revision": "c5d0a1925b3a97121b38",
+    "url": "/Path-Bingo/static/js/2.7c6855e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Path-Bingo/static/js/2.7c6855e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6365d4d693b64fd50ed6",
+    "url": "/Path-Bingo/static/js/main.8bd12a26.chunk.js"
+  },
+  {
+    "revision": "2d57edd5072111d5a1ef",
+    "url": "/Path-Bingo/static/js/runtime-main.67c98e5b.js"
+  }
+]);
